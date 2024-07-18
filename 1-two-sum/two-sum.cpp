85c206ca9a59d230bf4666573c8be04d9ea688bb
SnehaBakshi14 <snehabakshi1405@gmail.com>
2024-07-19 00:26:35 +0530

    Time: 13 ms (53.55%) | Memory: 14.5 MB (12.71%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -2,20 +2,33 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target)
     {
-        int n = nums.size();
-        unordered_map<int,int> map;
-        for(int i = 0;i<n;i++)
-        {
-            map[nums[i]] = i;//index ki mapping o_o
-        }
+        // int n = nums.size();
+        // unordered_map<int,int> map;
+        // for(int i = 0;i<n;i++)
+        // {
+        //     map[nums[i]] = i;//index ki mapping o_o
+        // }
 
-        for(int i =0;i<n;i++){
-            if(map.find((target - nums[i])) != map.end() && i != map[(target-nums[i])])
+        // for(int i =0;i<n;i++){
+        //     if(map.find((target - nums[i])) != map.end() && i != map[(target-nums[i])])
+        //     {
+        //         return {i,map[(target-nums[i])]};
+        //     }
+        // }
+        // return {-1,-1};
+
+        // Approach map
+        int n = nums.size();
+        map<int,int>mp;// no and its index
+        for(int i =0;i<n;i++)
+        {
+            int rem = target - nums[i];
+            if(mp.find(rem) != mp.end())
             {
-                return {i,map[(target-nums[i])]};
+                return {mp[rem],i};
             }
+            mp[nums[i]] = i;
         }
-        return {-1,-1};
-        
+        return {};
     }
 };
