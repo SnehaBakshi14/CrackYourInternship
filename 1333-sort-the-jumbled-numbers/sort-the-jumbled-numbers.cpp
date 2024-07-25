b2235ca99efdabab55dd9c00f83e64f569abe8ee
SnehaBakshi14 <snehabakshi1405@gmail.com>
2024-07-25 14:18:22 +0530

    Time: 405 ms (40.24%) | Memory: 88.8 MB (56.97%) - LeetSync

--- 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.cpp
+++ 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.cpp
@@ -1,34 +1,38 @@
 class Solution {
 public:
-    string  getmappedstr(string &num,vector<int>&mapping)
-    {
-        string mappedno = " ";
-        for(int i =0;i<num.length();i++)
-        {
-            char ch = num[i];
-            int it = ch - '0';
-            mappedno += to_string(mapping[it]);
+
+    string getMappedNum(string &num, vector<int>& mapping) {
+        string mappedNum = "";
+        for(int i = 0; i < num.length(); i++) {
+            char ch   = num[i];
+            mappedNum += to_string(mapping[ch-'0']);
         }
-        return mappedno;
+        return mappedNum;
     }
-    vector<int> sortJumbled(vector<int>& mapping, vector<int>& nums) 
-    {
+
+    vector<int> sortJumbled(vector<int>& mapping, vector<int>& nums) {
         int n = nums.size();
-        vector<pair<int,int>>vec;//{mappedno ,index}
-        for(int i =0;i<n;i++)
-        {
-            string num = to_string(nums[i]);
-            string mappedstr = getmappedstr(num,mapping);
-            int mappedno = stoi(mappedstr);
-            vec.push_back({mappedno,i});
+        vector<pair<int, int>> vec;
+
+        for(int i = 0; i < n; i++) {
+            string numStr     = to_string(nums[i]);
+            
+            string mappedStr  = getMappedNum(numStr, mapping);
+
+            int mappedNum     = stoi(mappedStr);
+
+            vec.push_back({mappedNum, i});
         }
-        sort(vec.begin(),vec.end());
-        vector<int>result;
-        for(auto &p:vec)
-        {
-            int orignalindex = p.second;
-            result.push_back(nums[orignalindex]);
+
+        sort(begin(vec), end(vec));
+        vector<int> result;
+
+        for(auto &p : vec) {
+            int originalNumIdx = p.second;
+            result.push_back(nums[originalNumIdx]);
         }
         return result;
     }
 };
+
+
