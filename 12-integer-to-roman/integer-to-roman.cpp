7794054ba92993b793c14674f00c4790d47837ba
SnehaBakshi14 <snehabakshi1405@gmail.com>
2026-01-24 23:38:31 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (68.16%) - LeetSync

---
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string intToRoman(int num) 
+    {
+        static vector<int>val = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        static vector<string>sym = {"M","CM" ,"D","CD","C" ,"XC","L","XL" ,
+        "X" ,"IX" ,"V", "IV", "I"};
+        string res = "";
+        for(int i =0;i<13;i++)
+        {
+            if(num ==0) break;
+            int times = num/val[i];// no of times a particular value can be used in a number
+            while(times--)
+            {
+                res += sym[i];
+            }
+            num  = num%val[i];
+
+        }
+        return res;
+    }
+};
