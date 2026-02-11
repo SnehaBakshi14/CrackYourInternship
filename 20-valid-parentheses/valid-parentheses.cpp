92452989ea3452c21e8a9fa84537b6fff5ba08e5
SnehaBakshi14 <snehabakshi1405@gmail.com>
2026-02-11 18:16:54 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (38.06%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isValid(string s) 
+    {
+        stack<char>st;
+        int n = s.size();
+        for(int  i =0;i<n;i++)
+        {
+            if(s[i] == '{' || s[i] == '[' || s[i] == '(')
+            {
+                st.push(s[i]);
+            }
+            else 
+            {
+                if(st.empty()) return false ;// no corresponding opening bracket
+
+                char ch = st.top();
+       
+                if ((ch == '{' && s[i] == '}') ||
+                    (ch == '[' && s[i] == ']') ||
+                    (ch == '(' && s[i] == ')'))
+                {
+                          st.pop();
+                }
+                else return false;
+            }
+        }
+        return st.empty();
+    }
+};
