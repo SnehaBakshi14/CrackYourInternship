114a917d74687af0d600659ce2b73ea9fdf92414
SnehaBakshi14 <snehabakshi1405@gmail.com>
2026-02-12 20:16:54 +0530

    Time: 204 ms (55.39%) | Memory: 25.4 MB (52.24%) - LeetSync

---
+++ 4055-longest-balanced-substring-i/longest-balanced-substring-i.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool checkBalanced(vector<int>&freq)
+    {
+        int common =0;
+        for(int i =0;i<26;i++)
+        {
+            if(freq[i] == 0)continue;
+            if(common == 0)
+            {
+                common = freq[i];
+            }
+            else if(freq[i] != common)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    int longestBalanced(string s)
+    {
+        int n = s.size();
+        int maxlen =0;
+        for(int i =0;i<n;i++)
+        {
+            vector<int>freq(26, 0);
+            for(int j =i;j<n;j++)
+            {
+                freq[s[j] - 'a']++;
+                if(checkBalanced(freq))
+                {
+                    maxlen = max(maxlen , j-i+1);
+                }
+            }
+        }
+        return maxlen;
+    }
+    // TC 0(N^2)
+    // sc o(26)
+};
