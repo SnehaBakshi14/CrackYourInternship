66a31c534e024650f93406412df9a4dc48d2316e
SnehaBakshi14 <snehabakshi1405@gmail.com>
2026-03-05 23:44:40 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (76.20%) - LeetSync

---
+++ 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int minOperations(string s) 
+    {
+        int n = s.length();
+        int start_with_0 =0; // 010101... format where odd position has 1 and even has 0
+        int start_with_1 =0 ; // 101010 
+        for(int i = 0;i<n;i++)
+        {
+            if(i %2 ==0 )// even
+            {
+                if(s[i] == '0')
+                {
+                    start_with_1++;
+                }else
+                {
+                    start_with_0 ++;
+                }
+            }
+            else
+            {
+                //odd
+                if(s[i] == '1')
+                {
+                    start_with_1++;
+                }
+                else{
+                    start_with_0 ++;
+                }
+            }
+        }
+        return min(start_with_0 , start_with_1);
+    }
+};
