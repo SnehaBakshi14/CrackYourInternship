d23cbc6272ad9a8a185399a1e4c3b974493965b8
SnehaBakshi14 <snehabakshi1405@gmail.com>
2026-04-25 00:21:06 +0530

    Time: 0 ms (100.00%) | Memory: 52.4 MB (56.20%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -3,18 +3,14 @@ public:
     bool canJump(vector<int>& nums) 
     {
         int n = nums.size();
-        int maxindx = 0;
+        int maxIndex = 0;
         for(int i =0;i<n;i++)
         {
-            if(i > maxindx)
-            {
-                return false;
-            }
-            if( maxindx <= n-1)
-            {
-                maxindx = max(maxindx , i+nums[i]);
-            }
+            if(i > maxIndex) return false; // you cannot reach an index greater than max index
+            maxIndex = max(maxIndex , (i + nums[i]));
+            if(maxIndex > n-1) break; // if crosses n that means you reached the end
         }
         return true;
     }
+    // TC O(N) SC 0(1)
 };
