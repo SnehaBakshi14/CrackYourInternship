09146a4c78cc5d6254af851329bdd7e296cd8b05
SnehaBakshi14 <snehabakshi1405@gmail.com>
2026-04-28 00:56:50 +0530

    Time: 4 ms (23.30%) | Memory: 112.5 MB (24.08%) - LeetSync

--- 134-gas-station/gas-station.cpp
+++ 134-gas-station/gas-station.cpp
@@ -3,20 +3,20 @@ public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) 
     {
         int n = gas.size();
-        int surplus =0;
-        int start =0;
-        int total_surplus;
+        int tank = 0; // to check if Can I reach next station from current start
+        int total = 0; // Is it even possible to complete the circle at all
+        int start = 0; // to find the starting index where circular route is complete 
         for(int i =0;i<n;i++)
         {
-            total_surplus += gas[i] - cost[i];
-            surplus += gas[i] - cost[i];
-            if(surplus < 0)
+            total += gas[i] - cost[i];
+            tank += gas[i]-cost[i];
+            if(tank < 0)
             {
-                surplus = 0;
+                tank =0;
                 start = i+1;
             }
-        
-        }        
-        return (total_surplus < 0 ) ? -1 : start;
+
+        }
+    return (total < 0) ? -1 : start;
     }
 };
