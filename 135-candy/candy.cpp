e25dfda5eeccd454de35d5061642f90f07a3d155
SnehaBakshi14 <snehabakshi1405@gmail.com>
2026-04-28 17:13:05 +0530

    Time: 4 ms (14.41%) | Memory: 22 MB (92.25%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -3,24 +3,33 @@ public:
     int candy(vector<int>& ratings) 
     {
         int n = ratings.size();
-        vector<int>l2r(n,1);
-        vector<int>r2l(n,1); // following rule 1
-        for(int i = 1;i<n;i++)
+        int candy = n; // Each child must have at least one candy.
+        int i =1 ;
+        while(i<n)
         {
-            if(ratings[i] > ratings[i-1])
-            l2r[i] = max(l2r[i],l2r[i-1]+ 1);
-        } 
-        for(int i = n-2;i>=0;i--) // if you add 1 in n-1 then you will go out of bound
-        {
-            if(ratings[i] > ratings[i+1])
-            r2l[i] = max(r2l[i],r2l[i+1]+ 1);
-        } 
-        int res =0;
-        for(int i =0;i<n;i++)
-        {
-            res += max(l2r[i] , r2l[i]);
+            if(ratings[i] == ratings[i-1])
+            {
+                i++;
+                continue;
+            }
+            int peak =0; // increasing slope
+            // new peak for every mountain
+            while(ratings[i] > ratings[i-1])
+            {
+                peak++;
+                candy += peak;
+                i++;
+                if(i == n) return candy;
+            }
+            int dip = 0; // decreasing slope 
+            while(i<n && ratings[i] < ratings[i-1])
+            {
+                dip++;
+                candy += dip;
+                i++;
+            }
+            candy -= min(peak , dip); // both got added we will keep the maximum one
         }
-        return res;
-        
+        return candy;
     }
 };
